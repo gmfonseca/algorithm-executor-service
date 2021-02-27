2517606cd14927599fb0fc923bbce7ba153c1964
gmfonseca <gmagnofonseca@gmail.com>
2021-02-27 19:46:40 -0300

    - Migrate algorithms and proto to use git submodule

--- src/main/java/br/com/gmfonseca/tcc/algorithms/Utils.kt
+++
@@ -1,7 +0,0 @@
-package br.com.gmfonseca.tcc.algorithms
-
-fun <T> MutableList<T>.swap(indexA: Int, indexB: Int) {
-    val temp = this[indexA]
-    this[indexA] = this[indexB]
-    this[indexB] = temp
-}
